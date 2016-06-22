4c404f766315a5c34a22f24f8a4df7b5277b3f63
Arun Sharma <asharma@fb.com>
2016-06-21 18:24:37 -0700

    Add stubs for Deserialize

--- include/rocksdb/utilities/serialize.h
+++ include/rocksdb/utilities/serialize.h
@@ -2,11 +2,15 @@
 
 #include <stdint.h>
 #include <string>
+#include <rocksdb/slice.h>
 
 // Serialize type T into a byte comprable
 // string and append to out.
 template<typename T>
 void Serialize(T val, std::string& out);
+
+template<typename T>
+T Deserialize(rocksdb::Slice& in);
 
 // Specializations
 template<>
@@ -23,4 +27,16 @@ inline void Serialize(double val, std::string& out) {
 
 template<>
 inline void Serialize(std::string val, std::string& out) {
+}
+
+template<>
+inline int64_t Deserialize(rocksdb::Slice& in) {
+}
+
+template<>
+inline bool Deserialize(rocksdb::Slice& in) {
+}
+
+template<>
+inline double Deserialize(rocksdb::Slice& in) {
 }
